ae21ad39e69fff19cc16a8db7bebfd87f777a056
Viraj Mody <ac117139@avcol.school.nz>
2023-07-20 13:15:30 +1200

    created appointments controller

--- Hospital/Areas/Identity/Data/HospitalContext.cs
+++ Hospital/Areas/Identity/Data/HospitalContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Hospital.Models;
 
 namespace Hospital.Areas.Identity.Data;
 
@@ -19,4 +20,6 @@ public class HospitalContext : IdentityDbContext<HospitalUser>
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
     }
+
+    public DbSet<Hospital.Models.Appointment> Appointment { get; set; } = default!;
 }
